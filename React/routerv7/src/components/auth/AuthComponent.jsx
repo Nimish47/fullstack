39b89ab09d7f5df6b9b06eac66765ec024b0e0ad
Nimish47 <nimishnishant1995@gmail.com>
2026-06-02 16:04:47 +0530

    Improved global loader screen

--- React/routerv7/src/components/auth/AuthComponent.jsx
+++ React/routerv7/src/components/auth/AuthComponent.jsx
@@ -1,23 +1,13 @@
 import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
-import SuspenseLoader from "../loadscreens/suspenseloader/SuspenseLoader";
+import MainLoader from "../loadscreens/mainloader/mainloader";
 
 export default function AuthComponent() {
 
-    // why used?    
-    // useLoaderData();
-
-    const navigation = useNavigation();
-
-    // console.log('navigation: ', navigation.state)
-
-    if (navigation.state === "loading") return <SuspenseLoader />
-    return <Outlet />
-
-//   return (
-//     <>
-//       {navigation.state === "loading" && <SuspenseLoader />}
-//       <Outlet />
-//     </>
-//   );    
+  return (
+    <>
+      <MainLoader />
+      <Outlet />
+    </>
+  );    
 
 }
---
+++ React/routerv7/src/components/loadscreens/mainloader/mainloader.jsx
@@ -0,0 +1,13 @@
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
+import SuspenseLoader from "../suspenseloader/SuspenseLoader";
+
+export default function MainLoader() {
+
+    const navigation = useNavigation();
+
+    // console.log('navigation: ', navigation.state)
+
+    if (navigation.state === "loading") return <SuspenseLoader />
+    return null   
+
+}
